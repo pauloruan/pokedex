d010233c91dbc716b43411dcaa3306373ec3ec94
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-07-07 11:23:05 -0300

    feat: types created

--- src/types/pokemon.d.ts
+++ src/types/pokemon.d.ts
@@ -29,13 +29,106 @@ interface PokemonSprites {
   versions: Versions
 }
 
+interface Ability {
+  name: string
+  url: string
+}
+
+interface PokemonAbility {
+  is_hidden: boolean
+  slot: number
+  ability: Ability
+}
+
+interface Form {
+  name: string
+  url: string
+}
+
+interface Version {
+  name: string
+  url: string
+}
+
+interface GameIndice {
+  game_index: number
+  version: Version
+}
+
+interface Item {
+  name: string
+  url: string
+}
+
+interface Version {
+  name: string
+  url: string
+}
+
+interface VersionDetail {
+  rarity: number
+  version: Version
+}
+
+interface HeldItem {
+  item: Item
+  version_details: Array<VersionDetail>
+}
+
+interface Move {
+  name: string
+  url: string
+}
+
+interface VersionGroup {
+  name: string
+  url: string
+}
+
+interface MoveLearnMethod {
+  name: string
+  url: string
+}
+
+interface VersionGroupDetail {
+  level_learned_at: number
+  version_group: VersionGroup
+  move_learn_method: MoveLearnMethod
+}
+
+interface PokemonMove {
+  move: Move
+  version_group_details: Array<VersionGroupDetail>
+}
+
+interface Stat {
+  name: string
+  url: string
+}
+
+interface PokemonStat {
+  base_stat: number
+  effort: number
+  stat: Stat
+}
+
 type Pokemon = {
-  height: number
   id: number
   name: string
+  base_experience: number
+  height: number
+  is_default: boolean
   order: number
   weight: number
-  sprites: PokemonSprites
+  abilities: Array<PokemonAbility>
+  forms: Array<Form>
+  game_indices: Array<GameIndice>
+  held_items: HeldItem[]
+  location_area_encounters: string
+  moves: Array<PokemonMove>
+  species: Species
+  sprites: Sprites
+  stats: Array<PokemonStat>
   types: Array<PokemonSlotType>
 }
 
@@ -49,3 +142,22 @@ type Pokemons = {
 type PokemonsResponse = Pokemons | undefined
 
 type PokemonResponse = Pokemon | undefined
+
+type TypesColors = {
+  [key: string]: string
+}
+
+type RootLayoutProps = {
+  children: ReactNode
+}
+
+type GlobalProviderProps = RootLayoutProps
+
+type GridProps = {
+  pokemons: PokemonType[]
+}
+
+type LoadMoreButtonProps = {
+  onClick: () => void
+  disabled: boolean
+}
